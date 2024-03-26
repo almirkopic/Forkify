c2be4265edd2d70cf698e9e6485bc2164c7df383
Almir Kopic <133537874+almirkopic@users.noreply.github.com>
2024-03-26 17:03:26 +0100

    Update View.js

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -4,6 +4,9 @@ export default class View {
   _data;
 
   render(data) {
+    if (!data || (Array.isArray(data) && data.length === 0))
+      return this.renderError();
+
     this._data = data;
     const markup = this._generateMarkup();
     this._clear();
@@ -19,36 +22,33 @@ export default class View {
       <svg>
         <use href="${icons}#icon-loader"></use>
       </svg>
+    </div> `;
+    this._parentElement.innerHTML = '';
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderError(message = this._errorMessage) {
+    const markup = `<div class="error">
+      <div>
+        <svg>
+          <use href="${icons}#icon-alert-triangle"></use>
+        </svg>
+      </div>
+      <p>${message}</p>
     </div>`;
-
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
 
-  renderMessage(message = this._errorMessage) {
-    const markup = ` <div class="message">
-              <div>
-                <svg>
-                  <use href="${icons}#icon-smile"></use>
-                </svg>
-              </div>
-              <p>${message}</p>
-            </div> `;
-
-    this._clear();
-    this._parentElement.insertAdjacentHTML('afterbegin', markup);
-  }
-
-  renderError(message = this._message) {
-    const markup = ` <div class="error">
-              <div>
-                <svg>
-                  <use href="${icons}#icon-alert-triangle"></use>
-                </svg>
-              </div>
-              <p>${message}</p>
-            </div> `;
-
+  renderMessage(message = this._message) {
+    const markup = `<div class="message">
+      <div>
+        <svg>
+          <use href="${icons}#icon-smile"></use>
+        </svg>
+      </div>
+      <p>${message}</p>
+    </div>`;
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
