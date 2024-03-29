a2d0e614789cd5e7c0cf3c5d5a9b739e4537934d
Almir Kopic <133537874+almirkopic@users.noreply.github.com>
2024-03-29 18:09:15 +0100

    Update resultsView.js

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -1,5 +1,6 @@
 import View from './View.js';
 import icons from 'url:../../img/icons.svg';
+import previewView from './previewView.js';
 
 class ResultsView extends View {
   _parentElement = document.querySelector('.results');
@@ -7,27 +8,7 @@ class ResultsView extends View {
   _message = '';
 
   _generateMarkup() {
-    return this._data.map(this._generateMarkupPreview).join('');
-  }
-  _generateMarkupPreview(result) {
-    return `
-    <li class="preview">
-            <a class="preview__link" href="#${result.id}">
-              <figure class="preview__fig">
-                <img src="${result.image}" alt="${result.title}" />
-              </figure>
-              <div class="preview__data">
-                <h4 class="preview__title">${result.title}</h4>
-                <p class="preview__publisher">${result.publisher}</p>
-                <div class="preview__user-generated">
-                  <svg>
-                    <use href="${icons}#icon-user"></use>
-                  </svg>
-                </div>
-              </div>
-            </a>
-          </li>
-    `;
+    return this._data.map(result => previewView.render(result, false)).join('');
   }
 }
 
